539ce1978d426c28519cdab64e92da253315d632
kkdroidgit <kartikdroid17@gmail.com>
2017-11-14 22:21:22 +0530

    Linked Project to Firebase

--- app/src/main/java/org/infpa/android/MainActivity.java
+++ app/src/main/java/org/infpa/android/MainActivity.java
@@ -1,23 +1,20 @@
 package org.infpa.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
 
     Drawer result = null;
-    private AccountHeader headerResult = null;
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +24,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar =  findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-
-        final IProfile profile = new ProfileDrawerItem().withName("Kartik Shandilya")
-                .withEmail("kartikdroid17@gmail.com").withIcon(R.mipmap.ic_launcher)
-                .withIdentifier(100);
-        final IProfile profile2 = new ProfileDrawerItem()
-                .withName("Demo User").withEmail("demo_user@gmail.com")
-                .withIcon(R.mipmap.ic_launcher).withIdentifier(101);
-
-        headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withHeaderBackground(R.drawable.header)
-                .withSavedInstance(savedInstanceState)
-                .withTranslucentStatusBar(true)
-                .addProfiles(profile,profile2,
-                        new ProfileSettingDrawerItem().withName("Add Account"),
-                        new ProfileSettingDrawerItem().withName("Manage Accounts")
-                )
-                .build();
-
         createNavDrawer(toolbar);
 
     }
 
-
     private void createNavDrawer(Toolbar toolbar) {
         //Create the drawer
         result = new DrawerBuilder()
-                .withAccountHeader(headerResult)
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withHasStableIds(true)
@@ -69,5 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 .build();
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
