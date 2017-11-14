b2737ed2ccf89484c9b7b2c8f3b8af098eac6d5f
kkdroidgit <kartikdroid17@gmail.com>
2017-11-14 22:56:00 +0530

    Firebase Authentication Added. The Providers are Google, Email and Phone Verification

--- app/src/main/java/org/infpa/android/MainActivity.java
+++ app/src/main/java/org/infpa/android/MainActivity.java
@@ -2,18 +2,31 @@ package org.infpa.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     Drawer result = null;
     private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseUser user;
+    String name;
+
 
 
     @Override
@@ -24,7 +37,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar =  findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
         createNavDrawer(toolbar);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //user is logged in
+                    name = user.getDisplayName();
+                }
+                else{
+                    //user is not logged in
+                    //create a list of providers i.e, Sign-In Methods
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build());
+                    //load a pre-built Auth UI
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                    RC_SIGN_IN);
+                }
+            }
+        };
+
 
     }
 
@@ -48,5 +92,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                //Login Successful
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Toast.makeText(this,"Logged In Successfully"+user.getDisplayName(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(resultCode == RESULT_CANCELED){
+                //Login Cancelled
+                Toast.makeText(this,"Login Cancelled! Try Again",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mAuthStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
